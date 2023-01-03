5b7dd753ca7e8268d68adf9e4696e78be1bdee1b
djx-ctrl <cras.dtravis@gmail.com>
2023-01-02 22:15:21 -0800

    completed assesment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,54 +14,199 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fibLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
+// RED:
 
-const fibLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+// describe("fibDouble", () => {
+//   it("takes in a number greater than 2 and returns length of numbers in sequence", () => {
+//     const fibLength1 = 6
+//     // Expected output: [1, 1, 2, 3, 5, 8]
+    
+//     const fibLength2 = 10
+//     // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+//     expect(fibDouble(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+//     expect(fibDouble(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+//   })
+// })
+// FAIL  ./code-challenges.test.js
+// fibDouble
+//   ✕ takes in a number greater than 2 and returns length of numbers in sequence
+
+// ● fibDouble › takes in a number greater than 2 and returns length of numbers in sequence
+
+//   ReferenceError: fibDouble is not defined
 
 // b) Create the function that makes the test pass.
+
+// GREEN:
+
+// PSEUDOCODE:
+// declare a function named fibDouble.
+// input: a number
+// output: returns an array with that length containing numbers of Fibonacci sequence
+
+// const fibDouble = (n) => {
+//     if (n==2) 
+//     {
+//       return [1, 1];
+//     } 
+//     else 
+//     {
+//       var s = fibDouble(n - 1);
+//       s.push(s[s.length - 1] + s[s.length - 2]);
+//       return s;
+//     }
+//   };
+//     console.log(fibDouble(6));
+//     console.log(fibDouble(10)); 
+//  PASS  ./code-challenges.test.js
+//  fibDouble
+//    ✓ takes in a number greater than 2 and returns length of numbers in sequence (1 ms)
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const studyMinutesWeek1 = {
-  sunday: 90,
-  monday: 30,
-  tuesday: 20,
-  wednesday: 15,
-  thursday: 30,
-  friday: 15,
-  saturday: 60
-}
-// Expected output: [15, 15, 20, 30, 30, 60, 90]
+// RED:
 
-const studyMinutesWeek2 = {
-  sunday: 100,
-  monday: 10,
-  tuesday: 45,
-  wednesday: 60,
-  thursday: 20,
-  friday: 15,
-  saturday: 65
-}
-// Expected output: [10, 15, 20, 45, 60, 65, 100]
+// describe("bothStudyMinutes", () => {
+//   it("takes in an object and return array sorted least to greatest", () => {
+//     const studyMinutesWeek1 = {
+//       sunday: 90,
+//       monday: 30,
+//       tuesday: 20,
+//       wednesday: 15,
+//       thursday: 30,
+//       friday: 15,
+//       saturday: 60
+//     }
+//     // Expected output: [15, 15, 20, 30, 30, 60, 90]
+    
+//     const studyMinutesWeek2 = {
+//       sunday: 100,
+//       monday: 10,
+//       tuesday: 45,
+//       wednesday: 60,
+//       thursday: 20,
+//       friday: 15,
+//       saturday: 65
+//     }
+//     // Expected output: [10, 15, 20, 45, 60, 65, 100]
+//     expect(bothStudyMinutes(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
+//     expect(bothStudyMinutes(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
+//   })
+// })
+// FAIL  ./code-challenges.test.js
+// bothStudyMinutes
+//   ✕ takes in an object and return array sorted least to greatest
+
+// ● bothStudyMinutes › takes in an object and return array sorted least to greatest
+
+//   ReferenceError: bothStudyMinutes is not defined
 
 // b) Create the function that makes the test pass.
+
+// GREEN:
+
+// PSEUDOCODE:
+// declare a function named bothStudyMinutes
+// input: an object
+// output: an array of values sorted from least to greatest
+
+// const studyMinutesWeek1 = {
+//   sunday: 90,
+//   monday: 30,
+//   tuesday: 20,
+//   wednesday: 15,
+//   thursday: 30,
+//   friday: 15,
+//   saturday: 60
+// }
+// const studyMinutesWeek2 = {
+//   sunday: 100,
+//   monday: 10,
+//   tuesday: 45,
+//   wednesday: 60,
+//   thursday: 20,
+//   friday: 15,
+//   saturday: 65
+// }
+
+// const bothStudyMinutes = (obj) => {
+//   let minuteArray = Object.values(obj)
+
+//   return minuteArray.sort((a,b) => {
+//     return a - b
+//   })
+// }
+
+// console.log(bothStudyMinutes(studyMinutesWeek1))
+// console.log(bothStudyMinutes(studyMinutesWeek2))
+// PASS  ./code-challenges.test.js
+// bothStudyMinutes
+//   ✓ takes in an object and return array sorted least to greatest (1 ms)
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const accountTransactions1 = [100, -17, -23, -9]
-// Expected output: [100, 83, 60, 51]
+// RED:
 
-const accountTransactions2 = [250, -89, 100, -96]
-// Expected output: [250, 161, 261, 165]
+describe("transactions5", () => {
+  it("takes in an array and returns array of accumlating sum", () => {
+    const accountTransactions2 = [250, -89, 100, -96]
+    // Expected output: [250, 161, 261, 165]
+    
+    const accountTransactions3 = []
+    // Expected output: []
+    expect(transactions5(accountTransactions2)).toEqual([250, 161, 261, 165])
+    expect(transactions5(accountTransactions3)).toEqual([])
+  })
+})
+// FAIL  ./code-challenges.test.js
+// transactions5
+//   ✕ takes in an array and returns array of accumlating sum
 
-const accountTransactions3 = []
-// Expected output: []
+// ● transactions5 › takes in an array and returns array of accumlating sum
+
+//   ReferenceError: transactions5 is not defined
 
 // b) Create the function that makes the test pass.
+
+// GREEN:
+
+// PSEUDOCODE:
+// declare a function named transactions5
+// input: an array of numbers
+// output: an array of the accumulating sum
+
+const accountTransactions2 = [250, -89, 100, -96]
+const accountTransactions3 = []
+
+const transactions5 = arr => arr.reduce((a, b, i) => i === 0 ? [b] : [...a, b + a[i-1]], 0);
+
+console.log(transactions5(accountTransactions2))
+console.log(transactions5(accountTransactions3))
+
+// Note: For the empty array I didnt figure out how to return [] instead of 0.
+// [ 250, 161, 261, 165 ]
+
+// at Object.log (code-challenges.test.js:188:9)
+
+// console.log
+// 0
+
+// at Object.log (code-challenges.test.js:189:9)
+
+// FAIL  ./code-challenges.test.js
+// transactions5
+// ✕ takes in an array and returns array of accumlating sum (2 ms)
+
+// ● transactions5 › takes in an array and returns array of accumlating sum
+
+// expect(received).toEqual(expected) // deep equality
+
+// Expected: []
+// Received: 0
+
+
--- interview-questions.md
+++ interview-questions.md
@@ -8,48 +8,49 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: From my understanding JSX is a mix of HTML and Javascript. JSX is a extension of Javascript.
 
-Researched answer:
+Researched answer: JSX is a extension of Javascript that gives a way to use component rendering using syntax similar to HTML. JSX gives the ability to write HTML elements in Javascript. One syntax difference is in HMTL <div></div> is used and in JSX fragments 
+<></> are used.
 
 2. What is yarn? What file(s) are modified in a React application when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: I don't recall from my own knowledge what is yarn or what files are modified in a React application when the command yarn is ran in terminal.
 
-Researched answer:
+Researched answer: Yarn is a package manager for Node.js. Originally created to address issues with the NPM package manager built by Meta.
 
 3. What is a React component?
 
-Your answer:
+Your answer: React components returns the value of JSX. In order to run a component call needs to be made. Also needs to have a self closing tag.
 
-Researched answer:
+Researched answer: A React component is a specific type of funciton containing Javascript logic and the return gets shown in the browser. In React, Functional components are used which are composed of Javascript functions.
 
 4. What is the difference between state values and props in React?
 
-Your answer:
+Your answer: State values determines behavior and Props take information to other components.
 
-Researched answer:
+Researched answer: The difference between state values and props in React are, state values are decbribed as special variables that store data and determine the behavior of the component. Components can either hold state or not hold state. As for props, they are objects used in React to pass information from component to component. They have unidirectional flow from the parent component to nested components. Also have a "read only" value.
 
 5. What is the DOM?
 
-Your answer:
+Your answer: DOM is what you see when a website pulls up in a browser.
 
-Researched answer:
+Researched answer: DOM is a visual reflection of the code. DOM also stands for Document Obect Model. In React, Virtual DOM is the key. The DOM changes a particular piece of the page and updates only the piece that has changed. For example, if someone likes a post on social media the virtual DOM reacts and the like button is now engaged.
 
 6. STRETCH: Which is the difference between a div and a span?
 
-Your answer:
+Your answer: A div divides content of the page and from my knowledge not entirely sure about span.
 
-Researched answer:
+Researched answer: A div or Division tag used in HTML to divide content on the web page(text, images, header,etc.). Div has both opening and closing tags. A span from my research is a inline container for inline elements. Groups elements for styling by using class or id attributes.
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: computer programming model that organizes software design around data, or objects rather than functions and logic.
 
-2. Ruby:
+2. Ruby: dynamic, open source programming language that focuses on simplicity plus productivity.
 
-3. Implicit return:
+3. Implicit return: funciton that returns values without using return as a keyword.
 
-4. Ruby blocks:
+4. Ruby blocks: lines of code that you put inside the do and end keywords.
 
-5. Ruby hashes:
+5. Ruby hashes: a data structure that is used to store data in unique key value pairs.
